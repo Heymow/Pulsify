dfb808be60ece3127facd897a194cde12807d587
Khaled CHEHAB <lynckit@hotmail.com>
2024-11-06 15:14:08 +0100

    Update CORS configuration to allow requests from the Pulsify web app

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ var genresRouter = require('./routes/genres');
 var app = express();
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors({ origin: 'https://pulsify-pink.vercel.app' }));
 
 app.use(logger('dev'));
 app.use(express.json());
