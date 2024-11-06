36faee891739c0b47f48b83ac85488c663ae08cb
Fragzel <fragzel@gmail.com>
2024-11-06 17:53:16 +0100

    Refactor CORS configuration in app.js

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,19 @@ var genresRouter = require('./routes/genres');
 var app = express();
 
 const cors = require('cors');
-app.use(cors({ origin: 'https://pulsify-pink.vercel.app' }));
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization'
+}));
 
 app.use(logger('dev'));
 // Augmenter la limite à 50 Mo
