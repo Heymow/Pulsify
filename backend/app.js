181fd0faed2785761e1322ff50fac3926bfeb51c
Fragzel <fragzel@gmail.com>
2024-11-06 17:49:39 +0100

    test

--- backend/app.js
+++ backend/app.js
@@ -19,10 +19,10 @@ var genresRouter = require('./routes/genres');
 var app = express();
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors({ origin: 'https://pulsify-pink.vercel.app' }));
 
 app.use(logger('dev'));
-// Augmenter la limite à 50 Mb
+// Augmenter la limite à 50 Mo
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
@@ -35,6 +35,11 @@ app.use('/projects', projectsRouter);
 app.use('/keywords', keywordsRouter);
 app.use('/genres', genresRouter);
 
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://pulsify-pink.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 module.exports = app;
